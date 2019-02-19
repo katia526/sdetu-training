23b97e0a3f15c8ba3e587c7efbf75b58b1184536
Katia <katia.verachtert@telenet.be>
2019-02-19 13:01:03 +0100

    Modified HelloWorld app

--- src/oop/IRate.java
+++ src/oop/IRate.java
@@ -1,12 +1,12 @@
 package oop;
 
 public interface IRate {
-	public void setRate() 
+	public void setRate() {
+		
+	}
 		
 	
-	public void increaseRate() 
-		
+	public void increaseRate() {
 	
-
-
+	
 }
